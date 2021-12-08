0759646b749b6ea8e834f2bb6bdf3bdc67486fce
arrazy100 <afdhalarrazy111@gmail.com>
2021-12-08 16:02:52 +0700

    implement character movement and dialogue system

---
+++ Assets/Maps/Test/04_Forest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="04_Forest" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <image source="Parallax Forest Background (Seamless)/Parallax Forest Background - Blue/04_Forest.png" width="1920" height="1080"/>
+</tileset>
