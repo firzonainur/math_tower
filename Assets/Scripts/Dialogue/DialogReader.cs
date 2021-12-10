ca2b14b0e26abbf38711c062d6dd2796d0ca0e40
arrazy100 <afdhalarrazy111@gmail.com>
2021-12-10 08:10:25 +0700

    Add dialog transition

--- Assets/Scripts/Dialogue/DialogReader.cs
+++ Assets/Scripts/Dialogue/DialogReader.cs
@@ -1,15 +1,20 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogReader : MonoBehaviour
 {
     public Sprite[] backgrounds;
+    public Image kotakDialog;
     public SpriteRenderer background;
     public Text characterName;
     public SpriteRenderer expressions;
     public Text message;
     public Dialogue story;
     public Sprite[] expressionSprites;
+    public float fadeTime;
+    public bool isFading = false;
     private int currentIndex = 0;
     private int maxIndex = 0;
 
@@ -24,12 +29,67 @@ public class DialogReader : MonoBehaviour
         else if (id == "") expressions.sprite = null;
     }
 
+    private IEnumerator fadeIn(float fadeTime, string charName, string msg)
+    {
+        Color tmp = background.color;
+        isFading = true;
+
+        hideDialog();
+        characterName.text = "";
+        message.text = "";
+
+        while (tmp.a < 1f)
+        {
+            tmp.a += Time.deltaTime / fadeTime;
+            background.color = tmp;
+
+            if (tmp.a > 1f) tmp.a = 1f;
+
+            yield return null;
+        }
+
+        background.color = tmp;
+        isFading = false;
+
+        if (msg != "") showDialog();
+        characterName.text = charName;
+        message.text = msg;
+    }
+
+    private void hideDialog()
+    {
+        Color tmp = kotakDialog.color;
+        tmp.a = 0f;
+        kotakDialog.color = tmp;
+    }
+
+    private void showDialog()
+    {
+        Color tmp = kotakDialog.color;
+        tmp.a = 0.9f;
+        kotakDialog.color = tmp;
+    }
+
     private void readMessage(int index)
     {
         var current = story.messages[index];
-        background.sprite = backgrounds[current.background];
-        characterName.text = current.characterName;
-        message.text = current.message;
+
+        if (background.sprite != backgrounds[current.background])
+        {
+            Color tmp = background.color;
+            tmp.a = 0f;
+            background.color = tmp;
+            background.sprite = backgrounds[current.background];
+
+            StartCoroutine(fadeIn(fadeTime, current.characterName, current.message));
+        }
+        else
+        {
+            background.sprite = backgrounds[current.background];
+            characterName.text = current.characterName;
+            message.text = current.message;
+        }
+
         setExpressions(current.expressions);
     }
 
@@ -43,7 +103,7 @@ public class DialogReader : MonoBehaviour
     {
         if (currentIndex < maxIndex)
         {
-            if (Input.GetKeyUp(KeyCode.Space))
+            if (Input.GetKeyUp(KeyCode.Space) && !isFading)
             {
                 currentIndex += 1;
                 readMessage(currentIndex);
